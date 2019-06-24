5d797dc934b33aafd52e50c3ff44f355ebdd129d
Lucas59356 <lucas59356@gmail.com>
2019-06-24 10:40:30 +0000

    Ignorando files.txt para testes e corrigindo chamadas no main que é o cara que testa a estrutura basica

--- main.c
+++ main.c
@@ -4,27 +4,27 @@
 int main() {
     struct tfs_node_t *root = NULL;
     tfs_node_mkdir(&root, &root, "/etc/init.d/eoq.conf");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     tfs_node_mkdir(&root, &root, "/etc/trab/eoq.conf");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     tfs_node_mkdir(&root, &root, "/etc/kappa/eoq.conf");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     tfs_node_mkdir(&root, &root, "/etc/ImageMagick-7/mime.xml");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     tfs_node_mkdir(&root, &root, "/tmp/eoq");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     tfs_node_mkdir(&root, &root, "/media/dados/Lucas");
     tfs_node_mkdir(&root, &root, "/media/dados/Jogos/Counter-Strike Xtreme V6/");
     tfs_node_mkdir(&root, &root, "/media/dados/Jogos/FlatOut 2");
     tfs_node_mkdir(&root, &root, "/media/dados/Jogos/GTA San Andreas");
-    print_node(root, 0);
+    tfs_node__print(root, 0);
     struct tfs_node_t **ret = tfs_node_chdir(&root, "media/dados/Jogos");
     assert(ret);
     assert(!tfs_node_chdir(&root, "/root"));
     assert(tfs_node_chdir(&root, "/media") == tfs_node_chdir(&root, "media")); // O / não pode dar diferença, de onde ele parte depende do root
-    print_node(*ret, 0);
+    tfs_node__print(*ret, 0);
     struct tfs_node_t **up = tfs_node_chdir(ret, "../../../etc");
     assert(*up);
-    print_node(*up, 0);
+    tfs_node__print(*up, 0);
     tfs_node__destroy(&root);
 }
